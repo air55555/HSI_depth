3ae1209291dd468e31eda130cec5e4270edd2c31
Alex <air55555@gmail.com>
2021-05-18 10:01:38 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,76 @@
+import imageio
+import glob
+import numpy as np
+import pandas as pd
+
+def combine_files():
+    """
+    Coombines sum files into single csv
+    :return:
+    """
+    df_sum = pd.DataFrame()
+    i=0
+    columns=['x']
+    for filepath in glob.iglob('sum*.txt'):
+        columns.append(filepath[3:9])
+        columns.append("t_"+filepath[3:9])
+        print(filepath)
+        df=pd.read_csv(filepath,header=None)
+        if i == 0:
+            df_sum[0]=df[0]
+        df_sum=pd.concat([df_sum,df[2]],axis=1 )
+        df_sum = pd.concat([df_sum, df[3]], axis=1)
+        i+=1
+    df_sum.columns=columns
+    pd.DataFrame.to_csv(df_sum,"sum.csv",index=None)
+
+def sum_lines(img,fname,koef):
+    """
+    writes summary data to .txt file representing the overall brightness of the
+    each of 1535 dots along all spectra. Also writes max value
+    """
+    res = []
+    if fname[0] == 't':
+        img_transformed= img * np.array(koef) [np.newaxis,: ]
+    else:
+        img_transformed = img
+    for j in range(0,1535 ):  # 1536
+        res.append((j,
+                    np.amax(img[j]),
+                    np.sum(img_transformed[j]),
+                    np.sum(img[j])
+                    ))
+    res = np.array(res)
+    np.savetxt( "sum" + fname + ".txt", res, fmt='%i', delimiter=',')
+
+
+
+def avg_spectra(fname):
+    """
+    writes summary data to .txt file representing the overall brightness of the
+    each of 1535 dots along all spectra. Also writes max value
+    """
+
+    img = imageio.imread(fname)
+    avg_all = int(np.average(img))
+    res = []
+    for j in range(0, 2048):  # 1536
+        avg = np.average(img[:, j])
+        res.append((j,
+                    avg,
+                    float(avg_all/avg)
+                      # abs max np.sum(img[j]) / 200
+
+                    ))
+    res = np.array(res)
+
+    np.savetxt( "avg" + fname + ".txt", res, fmt='%f', delimiter='\t',
+                header = "band\tbrightness\tkoef",comments='')
+    return res[:,2]
+
+koef=avg_spectra("led.tif")
+for filepath in glob.iglob('*.tif'):
+    if filepath == "led.tif": continue
+    img = imageio.imread(filepath)
+    sum_lines(img,filepath,koef)
+combine_files()
