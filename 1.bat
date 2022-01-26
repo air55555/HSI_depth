1d7e3ff5cfbe20095eabf021079c35a00ac9595c
air55555 <air55555@gmail.com>
2022-01-26 11:32:02 +0300

    fixes

--- 1.bat
+++ 1.bat
@@ -1,1 +1,2 @@
-C:\Users\LRS\PycharmProjects\HSI_depth\venv\Scripts\python.exe C:/Users/LRS/PycharmProjects/HSI_depth/main.py --dir s:\camera_out --lines 0 --get_only_tiff 0 --final 200
+--dir s:\bullet360 --lines 0 --get_only_tiff 555 --final 10 --external_img -360part1.jpg
+C:\Users\LRS\PycharmProjects\HSI_depth\venv36\Scripts\python.exe C:/Users/LRS/PycharmProjects/HSI_depth/main.py --dir s:\2021-12-08-14-32-37.0168307 --lines 0 --get_only_tiff 555 --final 200
