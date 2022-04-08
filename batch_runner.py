bb8d065fcf68613067237f1e7a1a2b76f883aad6
air55555 <air55555@gmail.com>
2022-04-08 10:53:33 +0300

    fixes

--- batch_runner.py
+++ batch_runner.py
@@ -6,22 +6,28 @@ config = ConfigParser()
 config.read(CONFIG_FILE)
 config.set('CONFIG',"start","0")
 config.set('CONFIG', "end", "704")
+config.set('CONFIG',"mid","300")
 with open(CONFIG_FILE, 'w') as configfile:
     config.write(configfile)
 
 config.read(CONFIG_FILE)
 start = int(config['CONFIG']["start"])
 end = int(config['CONFIG']["end"])
+mid = int(config.get('CONFIG',"mid"))
 run_string = config['CONFIG']["run_string"]
-mid = int((start+end)/2)
+mid_calculated = int((start+end)/2)
 
 
-for  i in range(20,300,20):
-    os.system(run_string)
-    config.set('CONFIG',"start",str(mid-i))
-    config.set('CONFIG', "end",str(mid+i))
-    with open(CONFIG_FILE, 'w') as configfile:
-        config.write(configfile)
+for j in range(10,abs(mid-mid_calculated),10):
+    mid_temp=j
+    for  i in range(20,300,20):
+
+        config.set('CONFIG',"start",str(mid_temp-i))
+        config.set('CONFIG', "end",str(mid_temp+i))
+        with open(CONFIG_FILE, 'w') as configfile:
+            config.write(configfile)
+        os.system(run_string)
+config.set('CONFIG',"mid","300")
 config.set('CONFIG',"start","0")
 config.set('CONFIG', "end", "704")
 with open(CONFIG_FILE, 'w') as configfile:
